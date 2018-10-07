50bcdaf1db31c69c7e8993aef8625bc1cbe75beb
michstallings <contact@michellestallings.com>
2018-10-07 14:00:43 -0700

    Created text nodes for each element

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ var pageBody = document.getElementsByTagName('body')[0];
 
 var scriptTag = document.getElementsByTagName('script')[0];
 
-pageBody.insertBefore(main,scriptTag)
+pageBody.insertBefore(main,scriptTag);
 
-var header = document.createElement('h1')
+var header = document.createElement('h1');
 
-main.appendChild(header)
+main.appendChild(header);
 
-var ul = document.createElement('ul')
+var ul = document.createElement('ul');
 
-main.appendChild(ul)
+main.appendChild(ul);
 
-var li = document.createElement('li')
+var li = document.createElement('li');
 
-ul.appendChild(li)
+ul.appendChild(li);
+
+var headerText = document.createTextNode('This is the header text!');
+
+var listItemText = document.createTextNode('This is the list item');
